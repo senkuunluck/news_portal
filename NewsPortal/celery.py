0c21504cba28e6418a23c056517b8f900ab8b673
senku <lektar4k7@gmail.com>
2024-02-14 15:40:22 +0300

    Initial commit

--- NewsPortal/celery.py
+++ NewsPortal/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPortal.settings')
 
@@ -7,3 +8,11 @@ app = Celery('NewsPortal')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'action_every_monday8am': {
+        'task': 'news.tasks.send_mail_every_monday8am',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+        'args': ()
+    }
+}
--- news/task.py
+++ news/task.py
@@ -1,9 +1,13 @@
+from datetime import datetime, timedelta
+
 from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.conf import settings
 
-from .models import PostCategory, Post
+from .models import PostCategory, Post, Category
+from ..NewsPortal.settings import EMAIL_HOST_USER
+
 
 @shared_task
 def new_post(pk):
@@ -32,4 +36,30 @@ def new_post(pk):
     )
 
     msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+@shared_task
+def weekly_post():
+    today = datetime.now()
+    last_week = today - timedelta(days=7)
+    posts = Post.objects.filter(datetime_post__gte=last_week)
+    cat = set(posts.values_list('category__name', flat=True))
+    subs = set(Category.objects.filter(name__in=cat).values_list('subscriber__email', flat=True))
+
+    from NewsPortal.NewsPortal.settings import SITE_URL
+    html_context = render_to_string(
+        'account/email/email_weekly_posts.html',
+        {
+            'head_link': f'{SITE_URL}',
+            'posts': posts,
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject=f'Последние посты за неделю',
+        body='',
+        from_email=EMAIL_HOST_USER,
+        to=subs,
+    )
+    msg.attach_alternative(html_context, 'text/html')
     msg.send()
